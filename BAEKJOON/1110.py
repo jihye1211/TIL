a183805042c9f471f8ddef6e6430e07f160ccd38
jihye1211 <44031523+jihye1211@users.noreply.github.com>
2022-12-21 13:22:19 +0900

    [BAEKJOON] 더하기 사이클
    개선하기

--- BAEKJOON/1110.py
+++ BAEKJOON/1110.py
@@ -1,17 +1,15 @@
-N = input()
-if int(N) < 10:
-    N = '0'+N
-    num = 0
-    sum = []
-    sum.append(N)
+N = int(input())
+num = N
+cnt = 0
 
-    while True:
-        a = str(int(sum[num][0])+int(sum[num][1]))
-        if int(a) < 10:
-            a = '0'+a
-            sum.append(sum[num][1]+a[1])
-            num += 1
-        if sum[num] == N:
-            break
-        print(num)
+while True:
+    a = num//10
+    b = num %10
+    c = (a+b)%10
+    num = (b*10) + c
 
+    cnt+=1
+    
+    if num == N:
+        print(cnt)
+        break
