304fe1a10a6b959f610761fa94d7d406c2329cf9
jihye1211 <44031523+jihye1211@users.noreply.github.com>
2023-08-08 01:30:26 +0900

    [BAEKJOON] TIL
    2023.08.08 일괄 커밋

---
+++ BAEKJOON/1002.py
@@ -0,0 +1,6 @@
+T = int(input())
+for i in range(T):
+    tastCase = input().split(' ')
+    -10000 <= tastCase[0,1,3,5] <=10000
+    tastCase[2,4] <= 10000
+    
---
+++ BAEKJOON/3003.py
@@ -0,0 +1,5 @@
+output= [0,0,0,0,0]
+count = input().split(' ')
+for i in range(len(count)):
+    if(count[i]=)
+
